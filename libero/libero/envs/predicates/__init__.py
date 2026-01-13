33acc487fb6c26b4753e40eefe4dc85e46fc5d83
yilin-wu98 <yilin-wu@outlook.com>
2026-01-13 15:23:50 -0800

    release

---
+++ libero/libero/envs/predicates/__init__.py
@@ -0,0 +1,55 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: MIT
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+from .base_predicates import *
+
+
+VALIDATE_PREDICATE_FN_DICT = {
+    "true": TruePredicateFn(),
+    "false": FalsePredicateFn(),
+    "in": In(),
+    "incontact": InContactPredicateFn(),
+    "on": On(),
+    "up": Up(),
+    # "stack":     Stack(),
+    # "temporal":  TemporalPredicate(),
+    "printjointstate": PrintJointState(),
+    "open": Open(),
+    "close": Close(),
+    "turnon": TurnOn(),
+    "turnoff": TurnOff(),
+}
+
+
+def update_predicate_fn_dict(fn_key, fn_name):
+    VALIDATE_PREDICATE_FN_DICT.update({fn_key: eval(fn_name)()})
+
+
+def eval_predicate_fn(predicate_fn_name, *args):
+    assert predicate_fn_name in VALIDATE_PREDICATE_FN_DICT
+    return VALIDATE_PREDICATE_FN_DICT[predicate_fn_name](*args)
+
+
+def get_predicate_fn_dict():
+    return VALIDATE_PREDICATE_FN_DICT
+
+
+def get_predicate_fn(predicate_fn_name):
+    return VALIDATE_PREDICATE_FN_DICT[predicate_fn_name.lower()]
